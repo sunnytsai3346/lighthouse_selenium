b06207aaf21bbf5db0bee7f40b3d64b2f455ba5a
Sunny Tsai <pl.sunnytsai@gmail.com>
2025-04-10 05:05:10 -0400

    approach to let lighthouse visit multiple urls

--- lighthouse-monitor-sub.py
+++ lighthouse-monitor-sub.py
@@ -1,17 +1,26 @@
 import subprocess
 
-url = "http://localhost:3000"  # Change to your actual target
-report_path ="./lighthouse-report-1.json"
 
-print(f"Auditing {url}...")
-NPX_CMD = r"C:\\nvm4w\\nodejs\\npx.cmd"   #where npx
-subprocess.run([
-    NPX_CMD, "lighthouse",
-    url,
-    "--port=9222",
-    "--output=json",
-    f"--output-path={report_path}",
-    "--quiet"
-], check=True)
+with open("visited_urls.txt") as f:
+    urls = [line.strip() for line in f if line.strip()]
 
-print(f"Lighthouse report saved to {report_path}")
+#url = "http://localhost:3000"  # Change to your actual target
+#report_path ="./lighthouse-report-1.json"
+for url in urls:
+    print(f"Auditing {url}...")
+    slug = url.replace("http://", "").replace("/", "_").strip("_")
+    output_path = f".\lighthouse_report\lighthouse-report-{slug}.json"
+    NPX_CMD = r"C:\\nvm4w\\nodejs\\npx.cmd"   #where npx
+    subprocess.run([
+        NPX_CMD, "lighthouse",
+        url,
+        "--port=9222",
+        "--output=json",
+        "--form-factor=mobile",
+        "--screenEmulation.width=360",
+        "--screenEmulation.height=640",
+        f"--output-path={output_path}",
+        "--quiet"
+    ], check=True)
+
+print(f"Lighthouse report saved lighthouse_report directory")
--- test_selenium.py
+++ test_selenium.py
@@ -9,6 +9,8 @@ chrome_options.add_argument("--remote-debugging-port=9222")  # Required for Ligh
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
 
+visited_urls = [] # this is for lighthouse
+
 try:
     # Step 2: Start WebDriver
     driver = webdriver.Chrome(options=chrome_options)
@@ -16,13 +18,23 @@ try:
     try:
         print("[**] Opening dummy app...")
         driver.get("http://localhost:3000")
+        visited_urls.append(driver.current_url)  ## need selenium to add driver.current_url to visited_urls
         time.sleep(5)  # Wait for page to load
+
+
+        # driver.get("http://localhost:3000/profile")
+        # visited_urls.append(driver.current_url)
+        # time.sleep(3)
 
         # You can add further actions/assertions here
         print("[**] Selenium test completed successfully.")
 
     finally:
         driver.quit()
+        # Save to file
+        with open("visited_urls.txt", "w") as f:
+            for url in visited_urls:
+                f.write(url + "\n")
 
 except WebDriverException as e:
     print(f"[❌] Selenium test failed. Stopping.\nError: {e}")
